46d16bd3901cccc7acc5b23c37c4e834a050ea02
Matheus Henrique <08990349931@edu.udesc.br>
2021-03-19 19:27:57 -0300

    iniciando projeto stock Trader

---
+++ stock-trader/src/components/stocks/Stock.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-flex class="pr-3 pb-3" xs12 md6 lg4>
+    <v-card class="green darken-3 white--text">
+      <v-card-title class="headline">
+        <strong
+          >{{ stock.name }}<small> (Preço: {{ stock.price }})</small></strong
+        >
+      </v-card-title>
+    </v-card>
+    <v-card>
+      <v-container fill-height>
+        <v-text-field
+          label="Quantidade"
+          type="number"
+          v-model.number="quantity"
+        />
+        <v-btn
+          :disabled="quantity <= 0 || !Number.isInteger(quantity)"
+          @click="buyStock"
+          class="green darken-3 white--text"
+          >Comprar</v-btn
+        >
+      </v-container>
+    </v-card>
+  </v-flex>
+</template>
+
+<script>
+export default {
+  props: ["stock"],
+  data() {
+    return {
+      quantity: 0,
+    };
+  },
+  methods: {
+    buyStock() {
+      const order = {
+        stockId: this.stock.id,
+        stockPrice: this.stock.price,
+        quantity: this.quantity,
+      };
+      this.quantity = 0;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
